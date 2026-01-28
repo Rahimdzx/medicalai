cd776914ad2a314611da89fdfd01c3c9d0059e4b
Mouissat Rabah abderrahmane <mouissatrabah@gmail.com>
2026-01-29 01:09:51 +0300

    MainActivity.kt

---
+++ android/app/src/main/kotlin/com/medicalns/app/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.medicalns.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // هذا الكود هو نقطة الانطلاق لتطبيق فلاتر على أندرويد
+    // تأكد أن السطر الأول (package) يطابق تماماً المعرف الخاص بك
+}
